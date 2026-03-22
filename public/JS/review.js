36f8d40d0239f641a42a13c551f629824ece07c5
Nikhil <xharmanikk9876@gmail.com>
2026-03-22 20:28:23 +0530

    fixed css/js path and static serving

--- public/JS/review.js
+++ public/JS/review.js
@@ -19,7 +19,7 @@ if (form) {
         };
 
         try {
-            const res = await fetch("http://localhost:5000/api/reviews", {
+            const res = await fetch("/api/reviews", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
